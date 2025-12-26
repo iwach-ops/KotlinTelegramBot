36221392353007fb4aed2c00828a2fe2e6722a5b
I Wachtel <iwachtel73@gmail.com>
2025-12-26 14:54:25 +0100

    ktb_05_develop_simulator_menu

---
+++ app/src/main/kotlin/ktb_iii_develop_simulator_menu/ktb_05_develop_simulator_menu.kt
@@ -0,0 +1,52 @@
+package org.example.app.ktb_iii_develop_simulator_menu
+
+import org.example.app.ktb_04_parse_print_dictionary_content.Word
+import java.io.File
+
+fun main() {
+    val dictionary = loadDictionary()
+
+    while (true) {
+        println(
+            """
+        Menu:
+        1 – Learn words
+        2 – Statistics
+        0 – Exit        
+    """.trimIndent()
+        )
+        val choice = readLine()?.toIntOrNull()
+
+        when (choice) {
+            1 -> println("Learn words")
+            2 -> println("Statistics")
+            0 -> break
+            else -> println("Enter the number 1, 2 or 0")
+        }
+    }
+}
+
+fun loadDictionary(): List<Word> {
+    val dictionaryFile = File("words.txt")
+    dictionaryFile.createNewFile()
+
+    val dictionary = mutableListOf<Word>()
+
+    dictionaryFile.readLines().forEach {
+        val line = it.split("|")
+        val word = line.getOrNull(0)?.trim().orEmpty()
+        val translate = line.getOrNull(1)?.trim().orEmpty()
+        val correctAnswersCount = line.getOrNull(2)?.trim()?.toIntOrNull() ?: 0
+
+        val wordElement = Word(word, translate, correctAnswersCount)
+
+        dictionary.add(wordElement)
+    }
+    return dictionary
+}
+
+data class Word(
+    val word: String,
+    val translate: String,
+    val correctAnswersCount: Int = 0,
+)
