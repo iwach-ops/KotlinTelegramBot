f623390d4ffcedf5455b653d9c40d3fe95e4da55
I Wachtel <iwachtel73@gmail.com>
2026-02-05 17:35:02 +0100

    ktb_19 process user responses and recopies

---
+++ app/src/main/kotlin/ktb_19_process_user_responses/Statistic.kt
@@ -0,0 +1,9 @@
+package org.example.app.ktb_19_process_user_responses
+
+data class Statistic(
+    val totalCount: Int,
+    val learnedCount: Int,
+    val percent: Int,
+){
+    fun printFormat(): String = "Learned ${learnedCount} from $totalCount words | $percent%"
+}
