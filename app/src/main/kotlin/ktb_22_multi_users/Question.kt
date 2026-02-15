91e9511453b365ae4d26e16defecdc9b556f16b1
I Wachtel <iwachtel73@gmail.com>
2026-02-15 20:03:14 +0100

    ktb_22 multi users and recopies

---
+++ app/src/main/kotlin/ktb_22_multi_users/Question.kt
@@ -0,0 +1,17 @@
+package org.example.app.ktb_22_multi_users
+
+data class Question(
+    val options: List<Word>,
+    val correctAnswer: Word,
+    val correctAnswerId: Int,
+)
+
+fun Question.asConsoleString(){
+    println()
+    println("${correctAnswer.word}:")
+    options.forEachIndexed { index, word ->
+        println("${index + 1} - ${word.translate}")
+    }
+    println("--------------")
+    println("0 - Menu")
+}
