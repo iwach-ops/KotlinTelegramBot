9f506199276277124e275a65b623452aaf88c367
I Wachtel <iwachtel73@gmail.com>
2026-02-18 22:43:24 +0100

    ktb_25   unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
    // implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     implementation(project(":utils"))
+    testImplementation(kotlin("test"))
 }
 
 application {
